8c8eb124a46495dce6346bf630a027ee92899cde
Ubuntu <ubuntu@ip-172-31-25-202.ec2.internal>
2019-08-10 21:54:40 +0000

    adding order controller and customer model link

---
+++ assignment9/order/app/controllers/order_controller.rb
@@ -0,0 +1,62 @@
+class OrdersController < ApplicationController
+   
+   # GET /orders
+    def show
+        order = Order.find_by(id: params[:id])
+        if order.nil?
+           render json: {error: "Order not found. #{params[:id]}" }, status: 404
+        else
+            render json: order, status: 200
+        end
+    end
+    
+    # GET /orders
+    # search by customerId or email
+    def search
+        customerId = params['customerId']
+        email = params['email']
+        if !email.nil?
+            code, customer = Customer.getCustomerByEmail(email)
+            if code !=200
+                render json: {error: "Customer email not found. #{email}" }, status: 400
+                return
+            end
+            customerId = customer[:id]
+        end
+        orders = Order.where(customerId: customerId)
+        render json: orders, status: 200
+    end
+    
+    # POST /orders
+    # POST /orders.json
+    
+    def create
+        @order = Order.new
+        code, customer = Customer.getCustomerByEmail(params[:email])
+        
+        if code != 200
+            render json: {error: "Item id not found. #{params[:itemId] }" }, status: 400
+            return
+        end
+        
+        if item[:stockQty] <= 0
+            render json: {error: "Item is out of stock."}, status: 400
+            return
+        end
+        
+        @order.itemId = params[:itemId]
+        @order.description = item[:description]
+        @order.customerId = customer[:customerId]
+        @order.price = item[:price]
+        @order.award = customer[:award]
+        @order.total = @order.price - @order.award
+        
+        if @order.save
+            code = Customer.putOrder(@order)
+            code = Item.putOrder(@order)
+            render json: @order, status: 201
+        else
+            render json: @order.errors, status: 400
+        end
+    end
+end
---
+++ assignment9/order/app/models/customer.rb
@@ -0,0 +1,18 @@
+class Customer
+    include HTTParty
+    base uri "http://localhost:8081"
+    format :json
+    
+    def Customer.getCustomerByEmail(email)
+        response = get "/customers?email=#{email}", headers: {"ACCEPT" => "application/json"}
+        status = response.code
+        customer = JSON.parse response.body, symbolize_names: true
+        return status, customer
+    end
+    
+    def Customer.putOrder(order)
+        response = put "/customers/order", body: order.to_json, headers: {"CONTENT_TYPE" => "application/json"}
+        response.code
+    end
+    
+end
