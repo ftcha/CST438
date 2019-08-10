f862a532e53641cdcb92d0db00e971bc0f871a0d
Ubuntu <ubuntu@ip-172-31-25-202.ec2.internal>
2019-08-10 22:16:56 +0000

    updating order routes

--- assignment9/order/app/models/customer.rb
+++ assignment9/order/app/models/customer.rb
@@ -1,16 +1,16 @@
-class Customer
+class SendToCustomerApp
     include HTTParty
     base uri "http://localhost:8081"
     format :json
     
-    def Customer.getCustomerByEmail(email)
+    def self.getCustomerByEmail(email)
         response = get "/customers?email=#{email}", headers: {"ACCEPT" => "application/json"}
         status = response.code
         customer = JSON.parse response.body, symbolize_names: true
         return status, customer
     end
     
-    def Customer.putOrder(order)
+    def self.putOrder(order)
         response = put "/customers/order", body: order.to_json, headers: {"CONTENT_TYPE" => "application/json"}
         response.code
     end
--- assignment9/order/config/routes.rb
+++ assignment9/order/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/orders' => 'orders#create'
+  get '/orders/:id' => 'orders#show'
+  get '/orders' => 'orders#search'
 end
