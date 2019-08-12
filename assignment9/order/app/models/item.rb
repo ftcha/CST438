62b27f33dbc5bcc9559b8467c0abaf75c8cbd690
Ubuntu <ubuntu@ip-172-31-25-202.ec2.internal>
2019-08-12 20:35:48 +0000

    updating order to include item linking model

---
+++ assignment9/order/app/models/item.rb
@@ -0,0 +1,18 @@
+class SendToItemApp
+    include HTTParty
+    base uri "http://localhost:8082"
+    format :json
+    
+    def self.getItemById(id)
+        response = get "/items/#{id}", headers: {"ACCEPT" => "application/json"}
+        status = response.code
+        item = JSON.parse response.body, symbolize_names: true
+        return status, item
+    end
+    
+    def self.putOrder(order)
+        response = put "/items/order", body: order.to_json, headers: {"CONTENT_TYPE" => "application/json"}
+        response.code
+    end
+    
+end
