f201bb51a3af2576066344cdfba7a2b2b82e99f9
EC2 Default User <ec2-user@ip-172-31-37-43.us-east-2.compute.internal>
2019-08-13 04:49:48 +0000

    Add db file and removed some files

---
+++ assignment9/order/db/migrate/20190730025558_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.integer :itemId
+      t.string :description
+      t.integer :customerId
+      t.float :price
+      t.float :award
+      t.float :total
+
+      t.timestamps
+    end
+  end
+end
